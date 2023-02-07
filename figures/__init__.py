a2689bd70cad71d32766aa8a7b0183c1be5dbbea
User <polina@potok.in>
2023-02-07 18:07:31 +0300

    Initial commit

---
+++ figures/__init__.py
@@ -0,0 +1,10 @@
+from .circle.code import circle_area, circle_perimetr
+from .triangle.code import triangle_perimetr, triangle_area
+from .square.code import square_area, square_perimetr
+
+__all__ = ('circle_perimetr',
+           'circle_area',
+           'triangle_perimetr',
+           'triangle_area',
+           'square_perimetr',
+           'square_area')
---
+++ figures/circle/code.py
@@ -0,0 +1,8 @@
+from math import pi
+
+def_rad = 5
+
+def circle_perimetr(radius=def_rad):
+    return 2 * pi * radius
+def circle_area(radius=def_rad):
+    return pi * radius ** 2
